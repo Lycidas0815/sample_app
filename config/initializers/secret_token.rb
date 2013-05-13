13995ae78a83c5761dffc4ad24852e29e6b09c03
Lycidas <lycidas@linux-3dmv.site>
2013-05-13 14:44:05 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '86aed92531285ae8f062e2a6e0ec26d1dd0832bc72490c75504710c221e5f94ce5e1ce5bfe109b4f08a220e1fffdab8d5d0cfd0f5d51cefb5e4bddd0d3ee8d4b'
